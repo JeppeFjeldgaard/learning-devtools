162b97c93f6f42ffd8af93117aa43bceab1591dc
JeppeFjeldgaard <45915633+JeppeFjeldgaard@users.noreply.github.com>
2021-10-29 13:44:07 +0200

    changed

--- R/fbind.R
+++ R/fbind.R
@@ -3,15 +3,13 @@
 #' Create a new factor from two existing factors, where the new factor's levels
 #' are the union of the levels of the input factors.
 #'
-#' @param a
-#' @param b
+#' @param a factor
+#' @param b factor
 #'
 #' @return factor
 #' @export
-#'
 #' @examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
-#'
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
